ccdb01fedad455449ffb456107aa595bbe7581f2
Julia Galan <juliag@Julias-MacBook-Pro.local>
2019-08-03 11:12:38 -0500

    object list

---
+++ js/main.js
@@ -0,0 +1,299 @@
+players [
+    {
+        name: "Trae Young",
+        team: "Atlanta Hawks",
+        img
+    },
+    {
+        name: "Atlanta Hawks",
+        team: "Atlanta Hawks",
+        img
+    },
+    {
+        name: "Kemba Walker",
+        team: "Boston Cetlics",
+        img
+    },
+    {
+        name: "Boston Cetlics",
+        team: "Boston Celtics",
+        img
+    },
+    {
+        name: "Kyrie Irving",
+        team: "Brooklyn Nets",
+        img
+    },
+    {
+        name: "Brooklyn Nets ",
+        team: "Brookyln Nets",
+        img
+    },
+    {
+        name: "Terry Rozier",
+        team: "Charlotte Hornets",
+        img
+    },
+    {
+        name: "Charlotte Hornets",
+        team: "Charlotte Hornets",
+        img
+    },
+    {
+        name: "Zach Lavine",
+        team: "Chicago Bulls",
+        img
+    },
+    {
+        name: "Chicago Bulls",
+        team: "Chicago Bulls",
+        img
+    },
+    {
+        name: "Kevin Love",
+        team: "Cleveland Cavaliers",
+        img
+    },
+    {
+        name: "Cleveland Cavaliers",
+        team: "Cleveland Cavilers",
+        img
+    },
+    {
+        name: "Luka Doncic",
+        team: "Dallas Mavericks",
+        img
+    },
+    {
+        name: "Dallas Mavericks",
+        team: "Dallas Mavericks",
+        img
+    },
+    {
+        name: "Nikola Jokic",
+        team: "Denver Nuggets",
+        img
+    },
+    {
+        name: "Denver Nuggets",
+        team: "Denver Nuggets",
+        img
+    },
+    {
+        name: "Blake Griffin",
+        team: "Detroit Pistons",
+        img
+    },
+    {
+        name: "Detroit Pistons",
+        team: "Detroit Pistons",
+        img
+    },
+    {
+        name: "Stephen Curry",
+        team: "Golden State Warriors",
+        img
+    },
+    {
+        name: "Golden State Warriors",
+        team: "Golden State Warriors",
+        img
+    },
+    {
+        name: "James Harden",
+        team: "Houston Rockets",
+        img
+    },
+    {
+        name: "Houston Rockets",
+        team: "Houston Rockets",
+        img
+    },
+    {
+        name: "Victor Oladipo",
+        team: "Indiana Pacers",
+        img
+    },
+    {
+        name: "Indiana Pacers",
+        team: "Indiana Pacers",
+        img
+    },
+    {
+        name: "Kawhi Leonard",
+        team: "LA Clippers",
+        img
+    },
+    {
+        name: "LA Clippers",
+        team: "LA Clippers",
+        img
+    },
+    {
+        name: "Lebron James",
+        team: "Los Angeles Lakers",
+        img
+    },
+    {
+        name: "Los Angeles Lakers",
+        team: "Los Angeles Lakers",
+        img
+    },
+    {
+        name: "Jonas Valanciunas",
+        team: "Memphis Grizzlies",
+        img
+    },
+    {
+        name: "Memphis Grizzlies",
+        team: "Memphis Grizzlies",
+        img
+    },
+    {
+        name: "Jimmy Butler",
+        team: "Miami Heat",
+        img
+    },
+    {
+        name: "Miami Heat",
+        team: "Miami Heat",
+        img
+    },
+    {
+        name: "Giannis Antetokounmpo",
+        team: "Milwaukee Bucks",
+        img
+    },
+    {
+        name: "Milwaukee Bucks",
+        team: "Milwaukee Bucks",
+        img
+    },
+    {
+        name: "Karl-Anthony Towns",
+        team: "Minnesota Timberwolves",
+        img
+    },
+    {
+        name: "Minnesota Timberwolves",
+        team: "Minnesota Timberwolves",
+        img
+    },
+    {
+        name: "Zion Williamson",
+        team: "New Orleans Pelicans",
+        img
+    },
+    {
+        name: "New Orleans Pelicans",
+        team: "New Orleans Pelicans",
+        img
+    },
+    {
+        name: "Mitchell Robinson",
+        team: "New York Knicks",
+        img
+    },
+    {
+        name: "New York Knicks",
+        team: "New York Knicks",
+        img
+    },
+    {
+        name: "Chris Paul",
+        team: "Oklahoma City Thunder",
+        img
+    },
+    {
+        name: "Oklahoma City Thunder",
+        team: "Oklahoma City Thunder",
+        img
+    },
+    {
+        name: "Aaron Gordon",
+        team: "Orlando Magic",
+        img
+    },
+    {
+        name: "Orlando Magic",
+        team: "Orlando Magic",
+        img
+    },
+    {
+        name: "Joel Embiid",
+        team: "Philadelphia 76ers",
+        img
+    },
+    {
+        name: "Philadelphia 76ers",
+        team: "Philadelphia 76ers",
+        img
+    },
+    {
+        name: "Devin Booker",
+        team: "Phoenix Suns ",
+        img
+    },
+    {
+        name: "Phoenix Suns ",
+        team: "Phoenix Suns ",
+        img
+    },
+    {
+        name: "Damian Lillard",
+        team: "Portland Trail Blazers",
+        img
+    },
+    {
+        name: "Portland Trail Blazers",
+        team: "Portland Trail Blazers",
+        img
+    },
+    {
+        name: "De'Araron Fox ",
+        team: "Sacramento Kings",
+        img
+    },
+    {
+        name: "Sacramento Kings",
+        team: "Sacramento Kings",
+        img
+    },
+    {
+        name: "LaMarcus Aldridge",
+        team: "San Antonio Spurs",
+        img
+    },
+    {
+        name: "Kyle Lowry",
+        team: "Toronto Raptors",
+        img
+    },
+    {
+        name: "Toronto Raptors",
+        team: "Toronto Raptors",
+        img
+    },
+
+    {
+        name: "Donovan Mitchell",
+        team: "Utah Jazz",
+        img
+    },
+
+    {
+        name: "Utah Jazz",
+        team: "Utah Jazz",
+        img
+    },
+    {
+        name: "Bradley Beal",
+        team: "Washington Wizards",
+        img
+    },
+    {
+        name: "Washington Wizards",
+        team: "Washington Wizards",
+        img,
+    },
+ ]
